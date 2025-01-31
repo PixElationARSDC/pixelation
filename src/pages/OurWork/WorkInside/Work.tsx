97f9590eca05a562a14ece252a4f3f4e8d55e2a2
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-01-31 19:18:32 +0530

    Add Tasveer section to Work page with dynamic yearly data display

--- src/pages/OurWork/WorkInside/Work.tsx
+++ src/pages/OurWork/WorkInside/Work.tsx
@@ -1,6 +1,7 @@
 import { VStack, Container } from '@chakra-ui/react';
 import Photography from '@/components/OurWork/WorkInside/Photography/Photography';
 import Header from '@/components/OurWork/WorkInside/Header/workheader';
+import TasveerSection from '@/components/OurWork/WorkInside/Tasveer/Tasveer';
 
 const Work = () => {
   return (
@@ -8,6 +9,7 @@ const Work = () => {
       <VStack gap={12} align="stretch">
         <Header />
         <Photography />
+        <TasveerSection/>
       </VStack>
     </Container>
   );
