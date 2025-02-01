c3ba378a3641d25df7631b1b1f1fb0d2c9b747dc
PixElation <pixelation@arsd.du.ac.in>
2025-02-01 11:32:14 +0530

    Update Slider.tsx

--- src/components/OurWork/Slider/Slider.tsx
+++ src/components/OurWork/Slider/Slider.tsx
@@ -6,9 +6,9 @@ import 'swiper/swiper-bundle.css'
 import './Slider.css'
 
 const Slider: React.FC = () => {
-  const slides = [
-  "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqIroFYLJNUtJLwMNhrRFHeBSKGfE0gvVsim54",
-  "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiq4yxNluFw42nWEea1k5KJxb0B6Cz3VGPhUNTA",
+ const slides = [
+  "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqNUoBRStleU7R6HXWi9dPkLwb1QSsIzCmyrvT",
+  "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqPMXpOw92Wzp0sn8LgHEleQ6u7YJmfUXT9ARr",
   "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiq8k86td5mUP9MYpSBJtDEuNfh5k2bewlAIvxn",
   "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqjkY7mUq7lBCwoRX5LrMImTanvK3kpN0iDVuH",
   "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqseTqIkLbEqQ9NDeHkIUXZF8py6JvLAn0hS2a",
@@ -18,6 +18,7 @@ const Slider: React.FC = () => {
   "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqxVuxgE9W2qEF7lTIkW0tsznKRiJULVDjc3dN",
   "https://1sh6in86sy.ufs.sh/f/MZxlz3TQUfiqSkA4MRhrziP6YaghbsDl0F39o2AnMkyx5uQW"
 ];
+
 
 
   return (
