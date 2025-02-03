eff8fea118dce0aeaaf0d4d861bd54d461de472c
PixElation <pixelation@arsd.du.ac.in>
2025-02-03 22:32:06 +0530

    Update TeamImage.tsx

--- src/components/Aboutus/AboutUsInside/TeamImage/TeamImage.tsx
+++ src/components/Aboutus/AboutUsInside/TeamImage/TeamImage.tsx
@@ -5,7 +5,7 @@ const TeamImage = () => {
   return (
     <Box>
       <Image
-        src="https://eytim95w0m.ufs.sh/f/9E19w1sYoF24twh9bfcvCy7e1zNEja4VBns0hdMmcZDHwPKJ?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        src="https://eytim95w0m.ufs.sh/f/9E19w1sYoF242Jlf5hoGoZPf7JpsgeX8KB9ztOAEW4RbCFlD?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="PixElation Team"
         borderRadius="lg"
       />
