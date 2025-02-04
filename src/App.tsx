f288c0b30d582265ae7ccfcdb1ce712d925f2f4e
Harshit Shukla <hershitshukla35@gmail.com>
2025-02-05 00:43:03 +0530

    Enhance loading experience with Skeleton component and adjust HeroPage height

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import { Skeleton } from '@chakra-ui/react';
 
 const AboutUs = lazy(() => import('./pages/About/AboutInside/AboutUs'));
 const Work = lazy(() => import('./pages/OurWork/WorkInside/Work'));
@@ -9,7 +10,7 @@ const EventPage = lazy(() => import('./pages/Event/EventPage'));
 const App: React.FC = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Skeleton height="100vh" width={"100%"}/>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<AboutUs />} />
